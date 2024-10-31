a10f2264c51cc432315b534d256768d89795e082
kicm <123@qq.com>
2024-11-01 03:18:42 +0800

    开启热部署

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // 健康检查路由
 router.get('/health', (req, res) => {
-  res.status(200).json({ status: 'OK啊' });
+  res.status(200).json({ status: 'OK啊a 啊' });
 });
 
 module.exports = router; 
